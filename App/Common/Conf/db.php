dd690ba80ca406c8295da95c72725feec69d16cd
770881781 <770881781@qq.com>
2018-01-23 11:48:31 +0800

    20180123

--- App/Common/Conf/db.php
+++ App/Common/Conf/db.php
@@ -3,7 +3,7 @@
   'DB_HOST' => '127.0.0.1',
   'DB_PORT' => '3306',
   'DB_USER' => 'root',
-  'DB_PWD' => 'trust___MeILIU@!#@$@bEIjING',
+  'DB_PWD' => '',
   'DB_NAME' => 'isp_res',
   'DB_PREFIX' => 'ires_',
   'DB_CHARSET' => 'utf8',
--- App/Common/Conf/db_oa.php
+++ App/Common/Conf/db_oa.php
@@ -5,7 +5,7 @@ return array(
             'DB_HOST'=>'localhost',
             'DB_NAME'=>'isp_oa',
             'DB_USER'=>'root',
-            'DB_PWD'=>'trust___MeILIU@!#@$@bEIjING',
+            'DB_PWD'=>'',
             'DB_PORT'=>'3306',
             'DB_PREFIX'=>'ioa_',
         ),
